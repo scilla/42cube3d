ffed15aad0dd318d4a26dad8d41963087c297d8f
scilla <sr.cilla@gmail.com>
2022-02-07 11:49:57 +0100

    add apt update to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+sudo apt update
 sudo apt install gcc make clang lldb xorg libxext-dev libbsd-dev zlib1g-dev libssl-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev libgles2-mesa-dev
 export DISPLAY=localhost:0.0
